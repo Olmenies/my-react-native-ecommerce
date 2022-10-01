d4ce1b75557b2b5daefce7b2d68fac11742a0436
olmenies <olmedonies@gmail.com>
2022-10-01 13:56:11 -0300

    Added icons to TabBar

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,10 +1,13 @@
 //React components
 import React from 'react';
 //React Native components
+import {View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 //Navigators
 import CartNavigator from './CartNavigator';
 import ShopNavigator from './ShopNavigator';
+//Icons
+import { Ionicons } from '@expo/vector-icons';
 
 const BottomTab = createBottomTabNavigator();
 
@@ -21,10 +24,26 @@ const TabNavigator = () => {
             <BottomTab.Screen
                 name='ShopTab'
                 component={ShopNavigator}
+                options={{
+                    title:'Shop',
+                    tabBarIcon:({focused}) => (
+                        <View>
+                            <Ionicons name={focused ? 'home' : 'home-outline'} size={24} color="black" />
+                        </View>
+                    ),
+                }}
             />
             <BottomTab.Screen
                 name='CartTab'
                 component={CartNavigator}
+                options={{
+                    title:'Cart',
+                    tabBarIcon:({focused}) => (
+                        <View>
+                            <Ionicons name={focused ? 'cart' : 'cart-outline'} size={24} color="black" />
+                        </View>
+                    )
+                }}
             />
         </BottomTab.Navigator>
     );
