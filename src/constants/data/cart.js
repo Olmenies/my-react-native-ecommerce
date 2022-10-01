4639777727ad880ba3b9cf0513c4f1e3652a5e2a
olmenies <olmedonies@gmail.com>
2022-10-01 16:27:49 -0300

    Created cart mock data on cart.js

---
+++ src/constants/data/cart.js
@@ -0,0 +1,20 @@
+export const CART = [
+    {
+        id:1,
+        category:1,
+        name:'Pan baguette',
+        description:'Clásico pan francés',
+        weight:'150gr',
+        price:1600,
+        quantity:2,
+    },
+    {
+        id:2,
+        category:1,
+        name:'Pan ciabatta blanco',
+        description:'Pan de alta hidratación, miga alveolada y corteza crujiente',
+        weight:'800gr',
+        price:3000,
+        quantity:6,
+    },
+];
