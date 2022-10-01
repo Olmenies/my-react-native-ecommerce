8f7e60453bd184d756f3889b2fdfdfaa563e534e
olmenies <olmedonies@gmail.com>
2022-09-30 21:33:52 -0300

    Added mock data on /constants

---
+++ src/constants/data/categories.js
@@ -0,0 +1,27 @@
+export const CATEGORIES = [
+    {
+        id:1,
+        name:'Harinas puras',
+        color:'#896978',
+    },
+    {
+        id:2,
+        name:'Harinas integrales',
+        color:'#839791',
+    },
+    {
+        id:3,
+        name:'Harinas de trigo',
+        color:'#AAC0AF',
+    },
+    {
+        id:4,
+        name:'Harinas de maíz',
+        color:'#896978',
+    },
+    {
+        id:5,
+        title:'Harinas de arroz',
+        color:'#896978',
+    },
+];
---
+++ src/constants/data/products.js
@@ -0,0 +1,34 @@
+export const PRODUCTS = [
+    {
+        id:1,
+        category:1,
+        name:'Pan baguette',
+        description:'Clásico pan francés',
+        weight:'150gr',
+        price:1600,
+    },
+    {
+        id:2,
+        category:,
+        name:'Pan ciabatta blanco',
+        description:'Pan de alta hidratación, miga alveolada y corteza crujiente',
+        weight:'800gr',
+        price:3000,
+    },
+    {
+        id:3,
+        category:1,
+        name:'Pan hamburguesa',
+        description:'Textura suave, decorado con semillas de sésamo tostadas',
+        weight:'120gr',
+        price:500,
+    },
+    {
+        id:3,
+        category:2,
+        name:'Pan de molde integral de centeno',
+        description:'Pedilo entero o tajado, con o sin semillas',
+        weight:'900gr',
+        price:3300,
+    },
+];
